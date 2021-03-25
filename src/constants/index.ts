e8ecea5f98f35784992b114986f7594f2e3efcc8
Victor Ngo <ngo.victor20@gmail.com>
2021-03-25 00:07:00 -0400

    Updated breakpoints

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,9 @@
 export const breakpoints = {
-  minDesktopOrLaptop: "1024px",
+  minDesktopOrLaptop: "1023px",
   minBigScreen: "1921px",
-  maxTablet: "1024px",
-  maxMobile: "480px",
-  maxTabletOrMobile: "1224px",
+  maxTablet: "1025px",
+  maxMobile: "481px",
+  maxTabletOrMobile: "1225px",
 };
 
 const config = {
