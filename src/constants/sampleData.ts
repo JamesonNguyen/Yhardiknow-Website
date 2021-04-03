edd7e178f93257c28e700920bd0ed1b54c5760c6
Victor Ngo <ngo.victor20@gmail.com>
2021-04-03 01:37:23 -0400

    Updated links to episodes

--- src/constants/sampleData.ts
+++ src/constants/sampleData.ts
@@ -2,8 +2,8 @@ import { Episode } from "types";
 
 const data: Array<Episode> = [
   {
-    audioUrl: `https://docs.google.com/uc?export=download&id=1-YGtf9crGBypUoB-tFkQHLjlorCukp2v`,
-    //audioUrl: `https://docs.google.com/uc?export=download&id=0B10YzQnkmnnJajBRYVNaaGhrSHM`,
+    // audioUrl: `https://docs.google.com/uc?export=download&id=1-YGtf9crGBypUoB-tFkQHLjlorCukp2v`,
+    audioUrl: `https://docs.google.com/uc?export=download&id=0B10YzQnkmnnJajBRYVNaaGhrSHM`,
     description:
       "Your boys Dj Toonz, R1 and Goggles take on the world with first ever episode of the Yhardiknow Podcast. Get to know the homies and hopefully there's going to be many more to come. ",
     episodeName: "Pilot Episode",
@@ -11,8 +11,8 @@ const data: Array<Episode> = [
     imageUrl: "ep_p.png",
   },
   {
-    audioUrl: `https://docs.google.com/uc?export=download&id=18UqauY7Q5hiZM-3NDRNHnY-vm7bRa8eb`,
-    //audioUrl: `https://docs.google.com/uc?export=download&id=0B10YzQnkmnnJRWw4elNXaUJWek0`,
+    // audioUrl: `https://docs.google.com/uc?export=download&id=18UqauY7Q5hiZM-3NDRNHnY-vm7bRa8eb`,
+    audioUrl: `https://docs.google.com/uc?export=download&id=0B10YzQnkmnnJRWw4elNXaUJWek0`,
     description:
       "And we're back! Episode 1 kicks off with new segments including PORC (Pop Off On Recent Content) and the Ep. to Ep. challenges. Listen in as we reminisce on our youths when we were yutes.",
     episodeName: "Throwback Thursdays",
@@ -20,8 +20,8 @@ const data: Array<Episode> = [
     imageUrl: "ep_1.png",
   },
   {
-    audioUrl: `https://docs.google.com/uc?export=download&id=1EcWo9ChLNGYLdutharHBhXpBQ7gaHk2q`,
-    //audioUrl:
+    // audioUrl: `https://docs.google.com/uc?export=download&id=1EcWo9ChLNGYLdutharHBhXpBQ7gaHk2q`,
+    audioUrl:
       "https://docs.google.com/uc?export=download&id=0B10YzQnkmnnJOUJoaS1HMnlhSmc",
     description:
       ' *Disclaimer: the PORC segment on COVID-19 was included and discussed much before any of the quarantining measures were enacted. Apologies for discussion points that may sound outdated or irrelevant. We were told not to make an episode on style. We did it anyways. This episode features some tasty PORC, discussions on how much style we did not have and the much awaited "H" ',
